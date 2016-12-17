4adce5957d2086b1d85d7658990d508de4dcb07d
jmk2142 <jmk2142@tc.columbia.edu>
2016-12-16 20:56:11 -0500

    Code we worked on together

--- main.js
+++ main.js
@@ -7,3 +7,34 @@ function myStep1() {
   document.getElementById("step1");
 }
 onclick = myStep1();
+
+
+document.getElementById('myItems').addEventListener('click', function(event){
+
+	if (event.target.tagName === "BUTTON") {
+
+		if (event.target.classList.contains('gotItBtn')) {
+			event.target.parentElement.parentElement.nextElementSibling.classList.add('shownItem');
+		} else {
+
+			if (event.target.classList.contains('btn-danger')) {
+				// add showFeedback class
+				event.target.nextElementSibling.classList.add('showFeedback');
+			} else {
+				var buttonEl = event.target;
+				var itemEl = buttonEl.parentElement;
+				var nextItemEl = itemEl.nextElementSibling;
+
+				console.log(buttonEl);
+				console.log(itemEl);
+
+				nextItemEl.classList.add('shownItem');
+			}
+
+		}
+
+		// alert('show next item');
+	} else {
+		// do nothing
+	}
+});
