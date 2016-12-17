504321865f72087395a8f7d018c9f832ef9125d2
xg2242 <xg2242@tc.columbia.edu>
2016-12-16 20:00:54 -0500

    final
    final

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-
 //how to realzie click --> show the new step btn?
 //Write the function of btn showing when clicking on btn YES/NOT SURE;
-//listen to YES;
+//listen to YES or NotSure;
 //how to hide the content until clicking on the btn? DOM Animation included?
 
-function myStep1(){
-getElementById("step1");
+function myStep1() {
+  document.getElementById("step1");
 }
- onclick= myStep1();
+onclick = myStep1();
