2e643493ae472b2206da5737414b1cad0061fe32
xg2242 <xg2242@tc.columbia.edu>
2016-12-16 19:02:53 -0500

    final
    final

---
+++ main.js
@@ -0,0 +1,12 @@
+
+//how to realzie click --> show the new step btn?
+
+function myStep(){
+
+}
+ onclick= myStep();
+ 
+
+
+
+//how to hide the content until clicking on the btn? DOM Animation included?
